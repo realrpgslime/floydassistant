48f539ab6c35388e8ed439e069018976c1764a1e
realrpgslime <97910160+realrpgslime@users.noreply.github.com>
2022-03-21 15:31:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const channel = client.channels.cache.get('950319389851385860')
 
 const textchannel = client.channels.cache.get('955411238383390730') || client.channels.fetch('955411238383390730');
 
-
-
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 });
@@ -26,13 +24,127 @@ client.on('ready', () => {
     console.log(chan);
 });
 
+
+
+/* BLM */
 client.on("messageCreate", (message) => {
     if (message.content == "stop resisting"){
         message.reply("I can't breathe!")
     }
 })
 
+client.on("messageCreate", (message) => {
+    if (message.content == "do a flip"){
+        message.reply("https://cdn.discordapp.com/emojis/929937750684205086.gif?size=240&quality=lossless")
+    }
+})
 
+client.on("messageCreate", (message) => {
+    if (message.content == "tuxedo"){
+        message.reply("https://c.tenor.com/5tJefIiRVfkAAAAd/blm-george-floyd.gif")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "rap"){
+        message.reply("https://c.tenor.com/5tJefIiRVfkAAAAd/blm-george-floyd.gif")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "wanna play"){
+        message.reply("https://www.youtube.com/watch?v=q59vjFPwkN0")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "breathe"){
+        message.reply("https://www.youtube.com/watch?v=5Qn8zxhHs44")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "breathe"){
+        message.reply("https://www.youtube.com/watch?v=5Qn8zxhHs44")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "breath"){
+        message.reply("https://www.youtube.com/watch?v=5Qn8zxhHs44")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "oxygen"){
+        message.reply("https://www.youtube.com/watch?v=5Qn8zxhHs44")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "co2"){
+        message.reply("https://www.youtube.com/watch?v=5Qn8zxhHs44")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "breathing"){
+        message.reply("https://www.youtube.com/watch?v=5Qn8zxhHs44")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "breatheless"){
+        message.reply("https://www.youtube.com/watch?v=5Qn8zxhHs44")
+    }
+})
+
+
+client.on("messageCreate", (message) => {
+    if (message.content == "dollar"){
+        message.reply("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTExvcU8AmdSTj6VOWvQd8naAREvegdxECk4g&usqp=CAU")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "money"){
+        message.reply("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTExvcU8AmdSTj6VOWvQd8naAREvegdxECk4g&usqp=CAU")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "dance"){
+        message.reply("https://c.tenor.com/rK4ICxQnBYgAAAAd/george-floyd-dance.gif")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "floyd what about our generation"){
+        message.reply("https://www.youtube.com/watch?v=1f7i1HIm7-g")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "generation"){
+        message.reply("https://www.youtube.com/watch?v=1f7i1HIm7-g")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return false;
+
+    if (message.content.includes("@here") || message.content.includes("@everyone") || message.type == "REPLY") return false;
+
+    if (message.mentions.has(client.user.id)) {
+        message.channel.send("Hello there! https://cdn.discordapp.com/emojis/929937750684205086.gif?size=240&quality=lossless");
+    }
+});
+
+
+
+
+
+/* voicify */
 client.on("voiceStateUpdate", (oldVoiceState, newVoiceState) => { // Listeing to the voiceStateUpdate event
     if (newVoiceState.channel) { // The member connected to a channel.
         console.log(`${newVoiceState.member.user.tag} connected to ${newVoiceState.channel.name}.`);
@@ -42,5 +154,8 @@ client.on("voiceStateUpdate", (oldVoiceState, newVoiceState) => { // Listeing to
         client.channels.cache.get("955411238383390730").send('has stopped breathing');
     };
 });
+
+
+
 
 client.login(process.env.TOKEN)
